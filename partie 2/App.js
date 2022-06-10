55f1634fe33cfc0e218f1072da17b333ac6aa126
Gauvin jonathan <59934702+Jonathan97480@users.noreply.github.com>
2022-06-10 17:55:35 +0400

    add exo partie 2

---
+++ partie 2/App.js
@@ -0,0 +1,30 @@
+/* Etape 1 crée tableaux  vide en html qui doit avoir dans sont thead nom ,prénom ,classe ,professeur ,moyenne */
+
+
+
+/* Etape 2   crée tableau d'objet qui va contenir les des info avec la structure suivante  {nom:string,prénom:string , classe:string ,professeur:string,moyenne:number} le array doit avoir 5 objet minimum*/
+
+
+
+
+
+/* Etape 3  afficher les dans le tableaux Html avec du JS  */
+
+
+
+
+
+
+/* Etape 4 Ajouter un champ de recherche au-dessus du tableaux  */
+
+
+
+
+/* Etape 5 récupérait les valeur entrée dans le champ de recherche */
+
+
+
+
+
+/* Etape 6  quand on tape le nom d'un élevé ou d'un professeur faire une recherche dans le tableau */
+
