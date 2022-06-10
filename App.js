35144f2f923aff18195a9763e1de81d5f23d480a
Gauvin jonathan <59934702+Jonathan97480@users.noreply.github.com>
2022-06-10 15:36:45 +0400

    first exo

---
+++ App.js
@@ -0,0 +1,21 @@
+/* Etape 1 crée une variable de type erray qui va contenir des objet qui ressemble a{nom:string,age:number,sexe:tring } */
+
+
+
+
+
+/* Etape 2 affiche les info sous forme de liste en utilisant ul présent dans html du projet  */
+
+
+
+
+
+
+/* Etap2 3 créé input de recherche que tu va injecter dans le dom en js avec un appenchild  */
+
+
+
+
+
+
+/* Etape   4 utiliser la propriété du type array qui permet de recherche avec un filter les éléments qui correspond a se que utilisateurs va entrée dans l'input de recherche   */
